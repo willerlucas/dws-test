74c9637d370230bc1254b49dc91fa37e791b0340
Willer Lucas <willerlbc@gmail.com>
2022-07-30 08:14:26 -0300

    initial commit

---
+++ src/main/java/io/github/willerlucas/dws/DwsTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.willerlucas.dws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/willerlucas/dws/DwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.willerlucas.dws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
